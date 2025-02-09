c15148c4db10070eb7494ef81a2c3c88516ada8c
Lars van Vianen <lars@vianen.com>
2025-02-09 21:45:25 +0100

    refactor demo

--- exe/demo/urls.py
+++ exe/demo/urls.py
@@ -25,7 +25,8 @@ Defines URL patterns for the demo project. This includes:
 
 # Import | Libraries
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.urls.resolvers import URLResolver
 
 # Import | Local Modules
 
@@ -34,8 +35,19 @@ from django.urls import path, include
 # URL Patterns
 # =============================================================================
 
-urlpatterns = [
-    path("admin/", admin.site.urls),  # Admin site URL
-    path("error/", include("swing_error.urls")),  # Include the URLs from the swing_error app
-    path("", include("swing_error.urls")),  # Include the URLs from the swing_error app
+urlpatterns: list[URLResolver] = [
+    path(
+        route="admin/",
+        view=admin.site.urls,
+    ),
+    path(
+        route="error/",
+        view=include(
+            arg="swing.error.urls",
+        ),
+    ),
+    path(
+        route="",
+        view=include(arg="swing.error.urls"),
+    ),
 ]
