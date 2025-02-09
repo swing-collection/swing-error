adcffd9e7d6dd811293102c57b4e2773b57192f2
Lars van Vianen <lars@vianen.com>
2025-02-09 21:48:20 +0100

    Update middleware_exception_logger.py

--- src/swing/error/middleware/middleware_exception_logger.py
+++ src/swing/error/middleware/middleware_exception_logger.py
@@ -1,13 +1,67 @@
+# -*- coding: utf-8 -*-
+
+# =============================================================================
+# Docstring
+# =============================================================================
+
+"""
+Provides Exception Logger Middleware Class
+==========================================
+
+
+"""  # noqa E501
+
+
+# =============================================================================
+# Imports
+# =============================================================================
+
+# Import | Standard Library
 import logging
+from typing import List
+
+# Import | Libraries
+
+# Import | Local Modules
+
+
+# =============================================================================
+# Logger
+# =============================================================================
+
+logger: logging.Logger = logging.getLogger(name=__name__)
+
+# =============================================================================
+# Class
+# =============================================================================
+
 
 class ExceptionLoggerMiddleware:
-    def __init__(self, get_response):
+    """ """
+
+    def __init__(self, get_response) -> None:
+        """ """
         self.get_response = get_response
-        self.logger = logging.getLogger('django')
+        self.logger: logging.Logger = logging.getLogger("django")
 
     def __call__(self, request):
+        """ """
         response = self.get_response(request)
         return response
 
-    def process_exception(self, request, exception):
-        self.logger.exception(str(exception))
+    def process_exception(
+        self,
+        request,
+        exception,
+    ) -> None:
+        """ """
+        self.logger.exception(str(object=exception))
+
+
+# =============================================================================
+# Exports
+# =============================================================================
+
+__all__: List[str] = [
+    "ExceptionLoggerMiddleware",
+]
